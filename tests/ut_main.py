ccc6f3f32184b6408a3792e5419560233f02d21f
Mandabee <33127053+Mandabee@users.noreply.github.com>
2017-10-31 12:03:11 -0500

    Huge work in progress
    I am not married to any of this so feel free to suggest things.
    
    I know the code is terrible, I know I should add more tests and split the ones I have into more classes.

--- tests/ut_main.py
+++ tests/ut_main.py
@@ -1,11 +1,48 @@
 import unittest
+import database
+import escavange
+import gameMaker
 
 class TestDatabaseCreation(unittest.TestCase):
   def setUp(self):
+    self.db = database.Database()
+    self.gameMaker = gameMaker.GameMaker()
+  
+  def accessDB(self):
     pass
+    #stub for now : check that we can access the database
+ 
+class TestMain(unittest.TestCase):
+  def setUp(self):
+    self.db = database.Database()
+    self.inputs = escavange.Main()
+    self.inputs.user = "Bob"            #Set the user inputs
+    self.inputs.password = "password"
+    
+  def testPrompt(self):
+    self.assertEquals(escavange.Main(), "Welcome to EScavange! Please log in: ", "Main function does not print expected value")
 
+  def testLoginNoTeams(self):
+    self.assertEquals(escavange.login(), "There is no such user. Please create a new team", "Login did not give the correct error message")
+
+  def testLoginTeam(self):
+    self.db.addTeam(user,password) #90% sure this is wrong
+    self.assertEquals(escavange.login(), "Currently logged in as Bob", "Login was not with the correct user")
+    
+  def testLoginMaster(self):
+    self.inputs.user = "maker"
+    self.inputs.password = "wrongpassword"
+    self.assertEquals(escavange.login(), "Failed to log in as Master", "Was able to log in as master and should not have been able to")
+    
+  def testLoginMaster(self):
+    self.inputs.user = "maker"
+    self.inputs.password = "password"
+    self.assertEquals(escavange.login(), "Logged in as Master", "Was NOT able to log in as master and should have been able to")
+  
 suite = unittest.TestSuite()
+
 suite.addTest(unittest.makeSuite(TestDatabaseCreation))
+suite.addTest(unittest.makeSuite(TestMain))
 
 runner = unittest.TextTestRunner()
 res=runner.run(suite)
