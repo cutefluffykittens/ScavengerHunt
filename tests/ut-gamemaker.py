7e1e7aa25e5e6179a4029f00616fc7ba6da460dc
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-10-31 11:17:50 -0500

    Fixed unit test modulability

--- tests/ut-gamemaker.py
+++ tests/ut-gamemaker.py
@@ -2,25 +2,37 @@ import unittest
 import gamemaker
 import database
 
-class TestLogin(unittest.TestCase):
+class TestMakerLogin(unittest.TestCase):
   def setUp(self):
     self.database = database.Database()
     self.maker1 = gamemaker.GameMaker()
   
   def test_makerLogin(self):
-     assertEquals(self.maker1.login("Notvalidusername","Notvalidpassword"), false, "test_makerLogin should not have logged in"))
-     assertEquals(self.database.curUser, null, "Current user is not null") //Make sure null is starting value!
-     assertEquals(self.maker1.login("maker","password"), true, "test_makerLogin should have logged in")
-     assertEquals(self.database.curUser, "maker", "Current user is not maker") //Make sure maker is what we will name it!
+     self.assertEquals(self.maker1.login("Notvalidusername","Notvalidpassword"), false, "test_makerLogin should not have logged in"))
+     self.assertEquals(self.database.curUser, null, "Current user is not null") //Make sure null is starting value!
+     self.assertEquals(self.maker1.login("maker","password"), true, "test_makerLogin should have logged in")
+     self.assertEquals(self.database.curUser, "maker", "Current user is not maker") //Make sure maker is what we will name it!
+   
+ class TestMakerCheckStatus(unittest.TestCase):
+  def setUp(self):
+    self.database = database.Database()
+    self.maker1 = gamemaker.GameMaker()
+    self.maker1.login("maker","password")
     
   def test_makerCheckStatus(self):
-     assertEquals(self.database.get_teams, null, "Get status is not null with an empty list of teams")
+     self.assertEquals(self.database.get_teams, null, "Get status is not null with an empty list of teams")
      self.database.add_teams("Team1")
-     assertEquals(self.database.get_teams, "Team1", "Get status is not Team1 when there is only Team1 in the list")
+     self.assertEquals(self.database.get_teams, "Team1", "Get status is not Team1 when there is only Team1 in the list")
      self.database.add_teams("team2")
-     assertEquals(self.database.get_teams, "Team1, team2", "Get status does not return Team1 and team2")
-  
-  def test_makerLogout(self):
-    maker1.logout()
-    assertEquals(self.database.curUser, null, "Current user is not null after logging out")
+     self.assertEquals(self.database.get_teams, "Team1, team2", "Get status does not return Team1 and team2")
+      
+  class TestMakerLogout(unittest.TestCase):
+    def setUp(self):
+      self.database = database.Database()
+      self.maker1 = gamemaker.GameMaker()
+      self.maker1.login("maker","password")
+    
+    def test_makerLogout(self):
+      self.maker1.logout()
+      self.assertEquals(self.database.curUser, null, "Current user is not null after logging out")
      
