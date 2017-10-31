f3cefb000f2a2a915fdd9a4309f7596f579f5563
Mandabee <33127053+Mandabee@users.noreply.github.com>
2017-10-31 11:16:30 -0500

    Update ut_main.py

--- tests/ut_main.py
+++ tests/ut_main.py
@@ -1,1 +1,14 @@
-insert things here
+import unittest
+
+class TestDatabaseCreation(unittest.TestCase):
+  def setUp(self):
+    pass
+
+suite = unittest.TestSuite()
+suite.addTest(unittest.makeSuite(TestDatabaseCreation))
+
+runner = unittest.TextTestRunner()
+res=runner.run(suite)
+print(res)
+print("*"*20)
+for i in res.failures: print(i[1])
