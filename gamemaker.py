1fb46e9bbcafe578edb2dbada44a53df4f0b591f
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-10-31 10:08:16 -0500

    Created new GameMaker file for the class

---
+++ gamemaker.py
@@ -0,0 +1,12 @@
+class GameMaker:
+  def __init__(self, database):
+    self.database = database
+    self.loggedin = false
+  
+  def login(self, name, password):
+    if (self.database.gameMakerCred = {name, password}):
+      self.loggedin = true
+  //TODO Implement checkStatus
+  def checkStatus(self):
+  //TODO Implement logout
+  def logout(name):
