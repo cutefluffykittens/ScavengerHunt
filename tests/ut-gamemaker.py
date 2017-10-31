0ec036004ecc4213e54a01d733146eed767d2730
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-10-31 10:45:54 -0500

    Added more tests

--- tests/ut-gamemaker.py
+++ tests/ut-gamemaker.py
@@ -2,4 +2,12 @@ import unittest
 import gamemaker
 import database
 
-class GameMaker
+class TestLogin(unittest.TestCase):
+  def setUp(self):
+    self.database = database.Database()
+    self.maker1 = gamemaker.GameMaker()
+  
+  def test_makerLogin(self):
+     assertEquals(self.maker1.login("Notvalidusername","Notvalidpassword"), false, "test_makerLogin should not have logged in"))
+     assertEquals(self.maker1.login("maker","password"), true, "test_makerLogin should have logged in"))
+    
