60793380759cb04f1c7a533edc66d4c4edb41f02
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-10-31 11:40:59 -0500

    Got rid of check status
    functionality changes

--- tests/ut-gamemaker.py
+++ tests/ut-gamemaker.py
@@ -12,19 +12,6 @@ class TestMakerLogin(unittest.TestCase):
      self.assertEquals(self.database.curUser, null, "Current user is not null") //Make sure null is starting value!
      self.assertEquals(self.maker1.login("maker","password"), true, "test_makerLogin should have logged in")
      self.assertEquals(self.database.curUser, "maker", "Current user is not maker") //Make sure maker is what we will name it!
-   
- class TestMakerCheckStatus(unittest.TestCase):
-  def setUp(self):
-    self.database = database.Database()
-    self.maker1 = gamemaker.GameMaker()
-    self.maker1.login("maker","password")
-    
-  def test_makerCheckStatus(self):
-     self.assertEquals(self.database.get_teams, null, "Get status is not null with an empty list of teams")
-     self.database.add_teams("Team1")
-     self.assertEquals(self.database.get_teams, "Team1", "Get status is not Team1 when there is only Team1 in the list")
-     self.database.add_teams("team2")
-     self.assertEquals(self.database.get_teams, "Team1, team2", "Get status does not return Team1 and team2")
       
   class TestMakerLogout(unittest.TestCase):
     def setUp(self):
@@ -38,7 +25,6 @@ class TestMakerLogin(unittest.TestCase):
      
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestMakerLogin))
-suite.addTest(unittest.makeSuite(TestMakerCheckStatus))
 suite.addTest(unittest.makeSuite(TestMakerLogout))
 
 runner = unittest.TextTestRunner()
