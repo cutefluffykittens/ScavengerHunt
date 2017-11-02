6097aa788378ce0839d138128c30bd44de72c552
Mandabee <33127053+Mandabee@users.noreply.github.com>
2017-11-01 21:02:23 -0500

    Update EScavange.py

--- EScavange.py
+++ EScavange.py
@@ -1,1 +1,11 @@
+import database.py
+import gamemaker.py
 
+class main():
+  
+  def __init__(self):
+    self.db = database.Database()
+    self.gameMaker = gameMaker.GameMaker()
+  
+  def login(self):
+    
