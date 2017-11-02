f0b8b82547207fb165a6adfd67b09eb10cd28a2d
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-11-01 22:18:49 -0500

    Added functionality
    Logout and Login

--- gamemaker.py
+++ gamemaker.py
@@ -1,13 +1,22 @@
 class GameMaker:
   def __init__(self, database):
     self.database = database
-    self.loggedin = false
   
   def login(self, name, password):
-    if (self.database.gameMakerCred = {name, password}):
-      self.loggedin = true
+    returnValue = ""
+    if (self.database.curUser != name && self.database.gameMakerCred = {name, password}):
+      self.database.curUser = name
+      returnValue = "User " + name + " logged in!"
+    else if (self.database.curUser == name) returnValue = "" + name + " already logged in!"
+    else returnValue = "Bad username or password!"
       
   //TODO Implement checkStatus
   def checkStatus(self):
-  //TODO Implement logout
-  def logout(name):
+    
+  def logout(self):
+    returnValue = ""
+    if (self.database.curUser != "maker") returnValue = "maker is not logged in!"
+    else {
+      self.database.curUser = null
+      returnValue = "maker logged out!"
+    return returnValue
