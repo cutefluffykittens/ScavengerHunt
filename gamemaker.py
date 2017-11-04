f52c6a59bd9a25172acb7c78602f9bb45eb05f95
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-11-04 13:01:43 -0500

    Implemented checkStatus method

--- gamemaker.py
+++ gamemaker.py
@@ -7,16 +7,18 @@ class GameMaker:
     if (self.database.curUser != name && self.database.gameMakerCred = {name, password}):
       self.database.curUser = name
       returnValue = "User " + name + " logged in!"
-    else if (self.database.curUser == name) returnValue = "" + name + " already logged in!"
-    else returnValue = "Bad username or password!"
-      
-  //TODO Implement checkStatus
-  def checkStatus(self):
+    elif: (self.database.curUser == name) returnValue = "" + name + " already logged in!"
+    else: returnValue = "Bad username or password!"
     
+  def checkStatus(self):
+    string = ' '.join(self.database.teams.keys())
+    if (string == ''): return "No teams!"
+    else: return string
+  
   def logout(self):
     returnValue = ""
-    if (self.database.curUser != "maker") returnValue = "maker is not logged in!"
-    else {
+    if (self.database.curUser != "maker"): returnValue = "maker is not logged in!"
+    else:
       self.database.curUser = null
       returnValue = "maker logged out!"
     return returnValue
