ba39a6f8803ccab5950d10b6a4463a989d7fc988
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-11-07 11:46:48 -0600

    Fixed syntax and check_status
    Worked with newfound wisdom of Pycharm and fixed check_status based on new criteria

--- gamemaker.py
+++ gamemaker.py
@@ -3,26 +3,29 @@ class GameMaker:
         self.database = database
 
     def login(self, name, password):
-        returnValue = False
+        return_value = False
         current_user = self.database.get_current_user()
         maker_cred = self.database.get_game_maker_cred()
-        if current_user == None:
+        if current_user is None:
             if maker_cred["maker"] == "maker" and maker_cred["password"] == password:
                 self.database.set_current_user(self)
-                returnValue = True
-        return returnValue
+                return_value = True
+        return return_value
 
     def check_status(self):
-        dict = self.database.teams.keys():
-        if len(dict) == 0:
-            return "No teams!"
+        string = ''
+        teams = self.database.get_teams()
+        if len(teams) > 0:
+            for team in teams:
+                string += team.user_name + ' is at ' + team.landmark + ' with ' + team.penalties + ' penalties\n'
         else:
-            return ' '.join(dict)
+            string = 'No teams!'
+        return string
         
     def logout(self):
-        returnValue = False
+        return_value = False
         current_user = self.database.get_current_user()
         if "maker" in current_user:
             self.database.set_current_user(None)
-            returnValue = True
-        return returnValue
+            return_value = True
+        return return_value
--- tests/ut-gamemaker.py
+++ tests/ut-gamemaker.py
@@ -5,53 +5,55 @@ import team
 import user
 
 class TestMakerLogin(unittest.TestCase):
-  def set_up(self):
-    self.database = database.Database()
-    self.maker1 = gamemaker.GameMaker()
-  
-  def test_bad_login(self):
-     self.assertEquals(self.maker1.login("Notvalidusername","Notvalidpassword"), False, "Bad response to invalid login")
-     self.assertEquals(self.database.get_current_user(), {}, "Current user is not null")
-  def test_good_login(self):
-     self.assertEquals(self.maker1.login("maker","password"), True, "Maker should have logged in")
-     self.assertEquals(self.database.get_current_user(), {"maker" : "password"}, "Current user should be maker")
-  def test_good_login_while_logged_in(self):
-    self.maker1.login("maker","password")
-    self.assertEquals(self.maker1.login("maker","password"), False, "Bad response to second login attempt")
-      
-  class TestMakerLogout(unittest.TestCase):
     def set_up(self):
-      self.database = database.Database()
-      self.maker1 = gamemaker.GameMaker()
+        self.database = database.Database()
+        self.maker1 = gamemaker.GameMaker()
+  
+    def test_bad_login(self):
+        self.assertEquals(self.maker1.login("Notvalidusername","Notvalidpassword"), False, "Bad response to invalid login")
+        self.assertEquals(self.database.get_current_user(), {}, "Current user is not null")
+    def test_good_login(self):
+        self.assertEquals(self.maker1.login("maker","password"), True, "Maker should have logged in")
+        self.assertEquals(self.database.get_current_user(), {"maker" : "password"}, "Current user should be maker")
+    def test_good_login_while_logged_in(self):
+        self.maker1.login("maker","password")
+        self.assertEquals(self.maker1.login("maker","password"), False, "Bad response to second login attempt")
+
+
+class TestMakerLogout(unittest.TestCase):
+    def set_up(self):
+        self.database = database.Database()
+        self.maker1 = gamemaker.GameMaker()
     
     def test_logout_while_not_logged_in(self):
-      self.assertEquals(self.maker1.logout(), False, "Bad response to maker logging out while not logged in")
+        self.assertEquals(self.maker1.logout(), False, "Bad response to maker logging out while not logged in")
     def test_good_logout(self):
-      self.maker1.login()
-      self.assertEquals(self.maker1.logout(), True, "Bad response to maker logging out correctly")
+        self.maker1.login()
+        self.assertEquals(self.maker1.logout(), True, "Bad response to maker logging out correctly")
     def test_current_user_is_not_maker(self):
-      self.maker1.login("maker","password")
-      self.maker1.logout()
-      self.assertEquals(self.database.get_current_user(), None, "Current user is not null after logging out")
-      
- class TestMakerCheckStatus(unittest.TestCase):
-  def set_up(self):
-    self.database = database.Database()
-    self.maker1 = gamemaker.GameMaker()
+        self.maker1.login("maker","password")
+        self.maker1.logout()
+        self.assertEquals(self.database.get_current_user(), None, "Current user is not null after logging out")
+
+
+class TestMakerCheckStatus(unittest.TestCase):
+    def set_up(self):
+        self.database = database.Database()
+        self.maker1 = gamemaker.GameMaker()
     
-  def test_empty_team(self):
-    self.assertEquals(self.maker1.check_status(), "No teams!", "CheckStatus did not return proper response to empty team list")
+    def test_empty_team(self):
+        self.assertEquals(self.maker1.check_status(), "No teams!", "CheckStatus did not return proper response to empty team list")
     
-  def test_single_team(self):
-    self.team1 = team.Team("Team1", "password", )
-    dict = self.maker1.check_status()
-    self.assertEquals(dict, "Team1", "Status incorrect for single team existing")
+    def test_single_team(self):
+        self.team1 = team.Team("Team1", "password", )
+        dict = self.maker1.check_status()
+        self.assertEquals(dict, "Team1", "Status incorrect for single team existing")
   
-  def test_multiple_teams(self):
-    self.team1 = team.Team("Team1", "password")
-    self.team2 = team.Team("team2", "password")
-    dict = self.maker1.check_status()
-    self.assertEquals(dict, "Team1 team2", "Cannot find entries in two team list")
+    def test_multiple_teams(self):
+        self.team1 = team.Team("Team1", "password")
+        self.team2 = team.Team("team2", "password")
+        dict = self.maker1.check_status()
+        self.assertEquals(dict, "Team1 team2", "Cannot find entries in two team list")
      
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestMakerLogin))
