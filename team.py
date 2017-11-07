e4f0405ca2abfb034124d5ed4b6426845ba9c08a
rodenbe4 <33126831+rodenbe4@users.noreply.github.com>
2017-11-06 18:22:49 -0600

    implemented Team class
    (mostly) implemented the Team class in its entirety

--- team.py
+++ team.py
@@ -1,17 +1,30 @@
 import user
 
 class Team(user.User):
-    def __init__(self,n):
-        self.name = n
-        self.username = "team"
+    def __init__(self,name):
+        self.name = name
+        self.username = "username"
         self.password = "password"
-    def setTeamName(self,n):
-        pass
-    def setTeamUserName(self,u):
-        pass
-    def setTeamPassword(self,p):
-        pass
+    def setTeamName(self,newName):
+        self.name = newName
+        return "Team name changed to " + newName
+    def setTeamUserName(self,newUsername):
+        self.username = newUsername
+        return "Team username changed to " + newUsername
+    def setTeamPassword(self,newPassword):
+        self.password = newPassword
+        return "Team password changed to " + newPassword
     def login(self,username,password):
-        pass
+        result = "Unable to log in"
+        # TODO: if someone is already logged in, just return result
+        if username == self.username and password == self.password:
+            # TODO: set currentUser to this user
+            result = self.name + " logged in"
+        return result
     def logout(self):
+        result = "Unable to log out"
+        # TODO: if someone is not logged in, or if team trying to log out is not the team currently logged in, return result
+        result = self.name + " logged out"
+        return result
+    def displayMenu(self):
         pass
--- tests/ut_team.py
+++ tests/ut_team.py
@@ -6,20 +6,30 @@ class TestTeamLogin(unittest.TestCase):
         self.team1 = team.Team("Name1")
         self.team2 = team.Team("Name2")
     def test_setTeamName(self):
-        self.assertEquals("Team name changed to n1", self.team1.setTeamName("n1"), "Error: changing team name failed")
+        self.team1.setTeamName("n1")
+        self.assertEquals("n1", self.team1.name, "Error: changing team name failed")
+        #self.assertEquals("Team name changed to n1", self.team1.setTeamName("n1"), "Error: changing team name failed")
     def test_setTeamUserName(self):
-        self.assertEquals("Team username changed to un1", self.team1.setTeamUserName("un1"), "Error: changing team user name failed")
+        self.team1.setTeamUserName("u1")
+        self.assertEquals("u1", self.team1.username, "Error: changing team user name failed")
+        #self.assertEquals("Team username changed to un1", self.team1.setTeamUserName("un1"), "Error: changing team user name failed")
     def test_setTeamPassword(self):
-        self.assertEquals("Team password changed to p1", self.team1.setTeamPassword("p1"), "Error: changing team password failed")
+        self.team1.setTeamPassword("p1")
+        self.assertEquals("p1", self.team1.password, "Error: changing team password failed")
+        #self.assertEquals("Team password changed to p1", self.team1.setTeamPassword("p1"), "Error: changing team password failed")
     def test_IncorrectLogin(self):
         self.assertEquals("Unable to log in",self.team1.login("wrong username","wrong password"), "Error: should not log in team1 with incorrect login info")
     def test_LoginFirstTeam(self):
+        self.team1.login("username, password");
         self.assertEquals("Name1 logged in",self.team1.login(self.team1.username,self.team1.password), "Error: failed to log in team1")
     def test_LoginSecondTeam(self):
+        self.team1.login("username","password")
         self.assertEquals("Unable to log in",self.team2.login(self.team2.username,self.team2.password), "Error: team1 is already logged in; cannot have two users logged in at once")
     def test_LogoutFirstTeam(self):
+        self.team1.login("username","password")
         self.assertEquals("Name1 logged out",self.team1.logout(), "Error: failed to log out team1")
     def test_LogoutSecondTeam(self):
+        self.team1.login("username","password")
         self.assertEquals("Unable to log out",self.team2.logout(), "Error: can't log out team2 when team1 is logged in")
     def test_LogoutNoTeamLoggedIn(self):
         self.assertEquals("Unable to log out",self.team1.logout(), "Error: cannot log out when no team is logged in")
