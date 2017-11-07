fad5a13a3d4b2b8b3eb4fb0033881674e70dd680
Mitchell Kossoris <mkossoris@gmail.com>
2017-11-06 18:43:17 -0600

    Changed syntax and added current_user

--- database.py
+++ database.py
@@ -1,24 +1,25 @@
 class Database:
     def __init__(self):
-        self.teamList = []
+        self.team_list = []
         self.landmarks = []
-        self.landmarkPath = []
-        self.guessPenalty = -1
-        self.timePenalty = -1
-        self.gameMakerCred = {"username":"username", "password":"password"}
+        self.landmark_path = []
+        self.guess_penalty = -1
+        self.time_penalty = -1
+        self.current_user = None
+        self.game_maker_cred = {"username":"maker", "password":"password"}
 
     def add_team(self, team):
-        self.teamList.append(team)
+        self.team_list.append(team)
 
     def delete_team(self, team):
         try:
-            self.teamList.remove(team)
+            self.team_list.remove(team)
 
         except ValueError:
             pass
 
     def get_teams(self):
-        return self.teamList
+        return self.team_list
 
     def add_landmark(self, landmark):
         self.landmarks.append(landmark)
@@ -34,29 +35,29 @@ class Database:
         return self.landmarks
 
     def add_to_path(self, landmark):
-        self.landmarkPath.append(landmark)
+        self.landmark_path.append(landmark)
 
     def delete_from_path(self, landmark):
         try:
-            self.landmarkPath.remove(landmark)
+            self.landmark_path.remove(landmark)
 
         except ValueError:
             pass
 
     def get_landmark_path(self):
-        return self.landmarkPath
+        return self.landmark_path
 
     def set_guess_penalty(self, penalty):
-        self.guessPenalty = penalty
+        self.guess_penalty = penalty
 
     def get_guess_penalty(self):
-        return self.guessPenalty
+        return self.guess_penalty
 
     def set_time_penalty(self, penalty):
-        self.timePenalty = penalty
+        self.time_penalty = penalty
 
     def get_time_penalty(self):
-        return self.timePenalty
+        return self.time_penalty
 
     def get_game_maker_cred(self):
-        return self.gameMakerCred
+        return self.game_maker_cred
