cf8c7dccda577bde5f9311d10e7a18c2c59e3cac
Mandabee <amandabruessl@gmail.com>
2017-11-06 19:59:47 -0600

    Changed to functionality discussed in group meeting

--- escavenge.py
+++ escavenge.py
@@ -1,18 +1,38 @@
 import database
 import gamemaker
+import team
 
 class Escavenge():
-  user = ""
-  password = ""
-  cmd = ""
-  response = ""
 
   def __init__(self):
-    self.db = database.Database()
-    self.gameMaker = gamemaker.GameMaker()
-  
-  def login(self):
-    pass
+    self.database = database.Database()
+    self.gameMaker = gamemaker.GameMaker(database)
+    self.user = team.Team(database)
+    maker = {}
+    team = {}
+
+    maker["logout"] = lambda: self.gameMaker.logout()
+    team["logout"] = lambda: self.user.logout()
+
+  def login(self,username, password):
+      if(username is "maker"):
+          self.gameMaker.login(username, password)
+      else:
+          self.user.login(username, password)
+
+  def main(self):
+
+    print("Welcome to Escavenge!\n")
+    while(True):
+        username = input("Please enter your username: ")
+        password = input("Please enter your password: ")
+
+        if(self.login(username, password)):
+            if(database.get_current_user() is "maker"):
+                cmd = input(self.gameMaker.display_menu())
+                self.maker[cmd]
+            else:
+                cmd = input(team.display_menu())
+                self.team[cmd]
 
 
-    
