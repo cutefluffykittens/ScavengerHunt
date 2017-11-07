d35b06e7c8e53a48bbb45f82409eb613fc4df1c0
cutefluffykittens <33108336+cutefluffykittens@users.noreply.github.com>
2017-11-06 19:18:03 -0600

    Cleaned up syntax

--- gamemaker.py
+++ gamemaker.py
@@ -1,24 +1,30 @@
 class GameMaker:
-  def __init__(self, database):
-    self.database = database
-  
-  def login(self, name, password):
-    returnValue = ""
-    if (self.database.curUser != name && self.database.gameMakerCred = {name, password}):
-      self.database.curUser = name
-      returnValue = "User " + name + " logged in!"
-    elif: (self.database.curUser == name) returnValue = "" + name + " already logged in!"
-    else: returnValue = "Bad username or password!"
-    
-  def checkStatus(self):
-    string = ' '.join(self.database.teams.keys())
-    if (string == ''): return "No teams!"
-    else: return string
-  
-  def logout(self):
-    returnValue = ""
-    if (self.database.curUser != "maker"): returnValue = "maker is not logged in!"
-    else:
-      self.database.curUser = null
-      returnValue = "maker logged out!"
-    return returnValue
+    def __init__(self, database):
+        self.database = database
+
+    def login(self, name, password):
+        returnValue = ""
+        if self.database.curUser.name != name and self.database.gameMakerCred == {name, password}:
+            self.database.curUser.name = name
+            returnValue = "User " + name + " logged in!"
+        elif self.database.curUser.name == name:
+            returnValue = "" + name + " already logged in!"
+        else:
+            returnValue = "Bad username or password!"
+        return returnValue
+
+    def check_status(self):
+        dict = self.database.teams.keys()
+        if None in dict:
+            return "No teams!"
+        else:
+            return dict
+
+    def logout(self):
+        returnValue = ""
+        if self.database.curUser != "maker":
+            returnValue = "maker is not logged in!"
+        else:
+            self.database.curUser = None
+            returnValue = "maker logged out!"
+        return returnValue
