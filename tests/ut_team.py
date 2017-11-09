9c812aa35f79d5a634ffae13d5bedb6cbbbc6e33
rodenbe4 <33126831+rodenbe4@users.noreply.github.com>
2017-11-08 18:51:19 -0600

    edited Team unittests
    all are passing and working on my computer

--- tests/ut_team.py
+++ tests/ut_team.py
@@ -4,8 +4,9 @@ import unittest
 
 class TestTeamLogin(unittest.TestCase):
     def setUp(self):
-        self.team1 = team.Team("teamname1","password1")
-        self.team2 = team.Team("teamname2","password2")
+        self.database = database.Database();
+        self.team1 = team.Team("teamname1","password1",self.database)
+        self.team2 = team.Team("teamname2","password2",self.database)
     def test_set_team_username(self):
         self.team1.set_team_username("u1")
         self.assertEquals("u1", self.team1.username, "Error: changing team user name failed")
@@ -13,24 +14,24 @@ class TestTeamLogin(unittest.TestCase):
         self.team1.set_team_password("p1")
         self.assertEquals("p1", self.team1.password, "Error: changing team password failed")
     def test_incorrect_login(self):
-        self.assertFalse(self.team1.login(database,"wrong username","wrong password"), "Error: should not log in team1 with incorrect login info")
+        self.assertFalse(self.team1.login("wrong username","wrong password"), "Error: should not log in team1 with incorrect login info")
     def test_login_first_team(self):
-        self.team1.login(database,"teamname1, password1")
-        self.assertTrue(self.team1.login(self.team1.username,self.team1.password), "Error: failed to log in team1")
+        #self.team1.login("teamname1, password1")
+        self.assertTrue(self.team1.login("teamname1","password1"), "Error: failed to log in team1")
     def test_login_second_team(self):
-        self.team1.login(database,"teamname1","password1")
-        self.assertFalse(self.team2.login(database,"teamname2","password2"), "Error: team1 is already logged in; cannot have two users logged in at once")
+        self.team1.login("teamname1","password1")
+        self.assertFalse(self.team2.login("teamname2","password2"), "Error: team1 is already logged in; cannot have two users logged in at once")
     def test_logout_first_team(self):
-        self.team1.login(database,"teamname1","password1")
-        self.assertTrue(self.team1.logout(database), "Error: failed to log out team1")
+        self.team1.login("teamname1","password1")
+        self.assertTrue(self.team1.logout(), "Error: failed to log out team1")
     def test_logout_second_team(self):
-        self.team1.login(database,"teamname1","password1")
-        self.assertFalse(self.team2.logout(database), "Error: can't log out team2 when team1 is logged in")
+        self.team1.login("teamname1","password1")
+        self.assertFalse(self.team2.logout(), "Error: can't log out team2 when team1 is logged in")
     def test_logout_no_team_logged_in(self):
-        self.assertFalse(self.team1.logout(database), "Error: cannot log out when no team is logged in")
+        self.assertFalse(self.team1.logout(), "Error: cannot log out when no team is logged in")
     def test_display_menu(self):
-        self.team1.login(database,"teamname1","password1")
-        self.assertEquals("Which would you like to do?\nlog out\ndisplay status",self.team1.display_menu(),"Error: incorrect menu displayed")
+        self.team1.login("teamname1","password1")
+        self.assertEquals("Options\n\nlog out\ndisplay status",self.team1.display_menu(),"Error: incorrect menu displayed")
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestTeamLogin))
