c878025d9ca658ebc48758f93feb74f2c2ee467b
rodenbe4 <33126831+rodenbe4@users.noreply.github.com>
2017-11-30 19:34:10 -0600

    changed Team() and team unit tests
    changed the current_landmark variable initialization to -1.  Refactored answer_question() and display_status() to account for this

--- tests/ut_team.py
+++ tests/ut_team.py
@@ -111,8 +111,8 @@ class TestTeamAnswerQuestions(unittest.TestCase):
         self.assertEqual("Correct answer given! You can now request the clue for the next landmark",
                          self.team1.answer_question(["a1"]),
                          "Error: should have returned True when answered correctly")
-        self.assertEqual(1,self.team1.current_landmark,
-                         "Error: current landmark was not updated from 0 to 1 when answer was correct")
+        self.assertEqual(0,self.team1.current_landmark,
+                         "Error: current landmark was not updated from -1 to 0 when answer was correct")
     def test_incorrect(self):
         self.team1.login("teamname1","password1")
         self.database.add_to_path(self.landmark1)
@@ -120,8 +120,8 @@ class TestTeamAnswerQuestions(unittest.TestCase):
         self.assertEqual("Incorrect answer, please try again",
                          self.team1.answer_question(["wrong answer"]),
                          "Error: should have returned False when answered incorrectly")
-        self.assertEqual(0,self.team1.current_landmark,
-                         "Error: current landmark should remain 0 when answer was incorrect")
+        self.assertEqual(-1,self.team1.current_landmark,
+                         "Error: current landmark should remain -1 when answer was incorrect")
     def test_correct_end_game(self):
         self.team1.login("teamname1","password1")
         self.database.add_to_path(self.landmark1)
@@ -131,10 +131,23 @@ class TestTeamAnswerQuestions(unittest.TestCase):
         self.assertEqual("Congratulations! Your team has won the game!",
                         self.team1.answer_question(["a2"]),
                         "Error: should have indicated that the team has won")
-        self.assertEqual(2,self.team1.current_landmark,
-                         "Error: current landmark was not updated from 1 to 2 when answer was correct")
+        self.assertEqual(1,self.team1.current_landmark,
+                         "Error: current landmark was not updated from 0 to 1 when answer was correct")
         self.assertFalse(self.database.game_running,
                          "Error: game_running should be changed from True to False once a team has won")
+
+class TestTeamInitialization(unittest.TestCase):
+    def setUp(self):
+        self.database = database.Database()
+        self.team1 = team.Team("username1","password1",self.database)
+    def test_initial_username(self):
+        self.assertEqual("username1",self.team1.username,"Error: username not initialized correctly")
+    def test_initial_password(self):
+        self.assertEqual("password1",self.team1.password,"Error: password not initialized correctly")
+    def test_initial_landmark(self):
+        self.assertEqual(-1,self.team1.current_landmark,"Error: current landmark should be initialized to -1")
+    def test_initial_penalties(self):
+        self.assertEqual(0,self.team1.penalties,"Error: penalties should be initialized to 0")
 
 suite = unittest.TestSuite()
 suite.addTest(unittest.makeSuite(TestTeamLogin))
